dc2af8f654d721863b4ce15888fd2c5b7210738a
Jesse Kane <jessekane66@gmail.com>
2026-03-31 12:46:20 -0400

    linting

--- photon-core/src/main/java/org/photonvision/vision/camera/FileVisionSource.java
+++ photon-core/src/main/java/org/photonvision/vision/camera/FileVisionSource.java
@@ -52,7 +52,8 @@ public class FileVisionSource extends VisionSource {
         if (getCameraConfiguration().cameraQuirks == null)
             getCameraConfiguration().cameraQuirks = QuirkyCamera.DefaultCamera;
 
-        // The getRobotToCamera method has a null check, so passing it to the FileFrameProvider should be safe even before the settables are initialized
+        // The getRobotToCamera method has a null check, so passing it to the FileFrameProvider should
+        // be safe even before the settables are initialized
         settables =
                 new FileSourceSettables(cameraConfiguration, frameProvider.get().frameStaticProperties);
     }
