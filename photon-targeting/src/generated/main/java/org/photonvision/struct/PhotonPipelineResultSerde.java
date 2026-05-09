f4e64eff7a88199dec234bb266d58bdae39b696c
Jesse Kane <jessekane66@gmail.com>
2026-05-09 16:50:51 -0400

    stuff

--- photon-targeting/src/generated/main/java/org/photonvision/struct/PhotonPipelineResultSerde.java
+++ photon-targeting/src/generated/main/java/org/photonvision/struct/PhotonPipelineResultSerde.java
@@ -33,6 +33,10 @@ import org.photonvision.utils.PacketUtils;
 // Assume that the base class lives here and we can import it
 import org.photonvision.targeting.*;
 
+// Needed for optional shims
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
 // WPILib imports (if any)
 import edu.wpi.first.util.struct.Struct;
 
@@ -86,7 +90,7 @@ public class PhotonPipelineResultSerde implements PacketSerde<PhotonPipelineResu
     @Override
     public PacketSerde<?>[] getNestedPhotonMessages() {
         return new PacketSerde<?>[] {
-            MultiTargetPNPResult.photonStruct,PhotonPipelineMetadata.photonStruct,PhotonTrackedTarget.photonStruct
+            MultiTargetPNPResult.photonStruct,PhotonTrackedTarget.photonStruct,PhotonPipelineMetadata.photonStruct
         };
     }
 
