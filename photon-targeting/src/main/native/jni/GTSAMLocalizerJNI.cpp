f4d775dea5198239348336f4c01f3a0c96290fc1
Jesse Kane <jessekane66@gmail.com>
2026-08-22 02:13:30 -0400

    format

--- photon-targeting/src/main/native/jni/GTSAMLocalizerJNI.cpp
+++ photon-targeting/src/main/native/jni/GTSAMLocalizerJNI.cpp
@@ -15,57 +15,68 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-
-#include <org_photonvision_jni_GTSAMLocalizer.h>
 #include <string>
 #include <vector>
+
+#include <org_photonvision_jni_GTSAMLocalizer.h>
 #include <wpi/apriltag/AprilTag.hpp>
-#include "photon/gtsam/FieldLayout.h"
-#include "photon/gtsam/Localizer.h"
 #include <wpi/apriltag/AprilTagFieldLayout.hpp>
 #include <wpi/units/length.hpp>
+
+#include "photon/gtsam/FieldLayout.h"
+#include "photon/gtsam/Localizer.h"
 
 extern "C" {
 
 /*
- * Class: org_photonvision_jni_GTSAMLocalizer
- * Method: create
+ * Class:     org_photonvision_jni_GTSAMLocalizer
+ * Method:    create
  * Signature: ([I[DDD[D)J
  */
 JNIEXPORT jlong JNICALL
 Java_org_photonvision_jni_GTSAMLocalizer_create
-    (JNIEnv* env, jclass, jintArray tagIDs, jdoubleArray tagPoses, jdouble fieldWidth, jdouble fieldLength, jdoubleArray tagCorners)
+  (JNIEnv* env, jclass, jintArray tagIDs, jdoubleArray tagPoses,
+   jdouble fieldWidth, jdouble fieldLength, jdoubleArray tagCorners)
 {
-    jsize tagIDsLength = env->GetArrayLength(tagIDs);
-    jsize tagCornersLength = env->GetArrayLength(tagCorners);
+  jsize tagIDsLength = env->GetArrayLength(tagIDs);
+  jsize tagCornersLength = env->GetArrayLength(tagCorners);
 
-    std::vector<wpi::apriltag::AprilTag> tags;
-    // TODO: Verify tag poses length is 6 * tagIDsLength
-    for (jsize i = 0; i < tagIDsLength; ++i) {
-        jint tagID = env->GetIntArrayElements(tagIDs, nullptr)[i];
-        jdouble* tagPosePtr = env->GetDoubleArrayElements(tagPoses, nullptr) + i * 6;
+  std::vector<wpi::apriltag::AprilTag> tags;
+  // TODO: Verify tag poses length is 6 * tagIDsLength
+  for (jsize i = 0; i < tagIDsLength; ++i) {
+    jint tagID = env->GetIntArrayElements(tagIDs, nullptr)[i];
+    jdouble* tagPosePtr =
+        env->GetDoubleArrayElements(tagPoses, nullptr) + i * 6;
 
-        wpi::math::Pose3d pose{
-            wpi::math::Translation3d(wpi::units::meter_t(tagPosePtr[0]), wpi::units::meter_t(tagPosePtr[1]), wpi::units::meter_t(tagPosePtr[2])),
-            wpi::math::Rotation3d(wpi::units::radian_t(tagPosePtr[3]), wpi::units::radian_t(tagPosePtr[4]), wpi::units::radian_t(tagPosePtr[5]))
-        };
+    wpi::math::Pose3d pose{
+        wpi::math::Translation3d(wpi::units::meter_t(tagPosePtr[0]),
+                                 wpi::units::meter_t(tagPosePtr[1]),
+                                 wpi::units::meter_t(tagPosePtr[2])),
+        wpi::math::Rotation3d(wpi::units::radian_t(tagPosePtr[3]),
+                              wpi::units::radian_t(tagPosePtr[4]),
+                              wpi::units::radian_t(tagPosePtr[5]))};
 
-        tags.emplace_back(tagID, pose);
-    }
+    tags.emplace_back(tagID, pose);
+  }
 
-    wpi::apriltag::AprilTagFieldLayout field(tags, wpi::units::meter_t(fieldWidth), wpi::units::meter_t(fieldLength));
-    
-    std::vector<wpi::math::Translation3d> verts;
-    for (jsize i = 0; i < tagCornersLength; i += 3) {
-        jdouble* cornerPtr = env->GetDoubleArrayElements(tagCorners, nullptr) + i;
-        verts.emplace_back(wpi::units::meter_t(cornerPtr[0]), wpi::units::meter_t(cornerPtr[1]), wpi::units::meter_t(cornerPtr[2]));
-    }
+  wpi::apriltag::AprilTagFieldLayout field(
+      tags, wpi::units::meter_t(fieldWidth), wpi::units::meter_t(fieldLength));
 
-    photon::TargetModel tagModel(verts);
+  std::vector<wpi::math::Translation3d> verts;
+  for (jsize i = 0; i < tagCornersLength; i += 3) {
+    jdouble* cornerPtr = env->GetDoubleArrayElements(tagCorners, nullptr) + i;
+    verts.emplace_back(wpi::units::meter_t(cornerPtr[0]),
+                       wpi::units::meter_t(cornerPtr[1]),
+                       wpi::units::meter_t(cornerPtr[2]));
+  }
 
-    photon::pvgtsam::FieldLayout fieldLayout = photon::pvgtsam::FieldLayout(field, tagModel);
-    photon::pvgtsam::Localizer* localizer = new photon::pvgtsam::Localizer(fieldLayout);
-    return reinterpret_cast<jlong>(localizer);
+  photon::TargetModel tagModel(verts);
+
+  photon::pvgtsam::FieldLayout fieldLayout =
+      photon::pvgtsam::FieldLayout(field, tagModel);
+  photon::pvgtsam::Localizer* localizer =
+      new photon::pvgtsam::Localizer(fieldLayout);
+  return reinterpret_cast<jlong>(localizer);
 }
 
-}
+}  // extern "C"
